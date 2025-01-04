953ac4c63d6174740475264689053e3fa7d992b5
Thandava krishna <96812377+Thandava3114@users.noreply.github.com>
2025-01-04 18:35:23 +0530

    Add files via upload

---
+++ Backend/NodeJS/Server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const cors = require("cors");
+const ACTIONS = require("./Actions");
+app.use(cors());
+const io = new Server(server, { cors: { origin: "*" } });
+
+const userSocketMap = {};
+function getAllConnectedClients(roomId) {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      };
+    }
+  );
+}
+
+io.on("connection", (socket) => {
+  console.log("Socket Connected", socket.id);
+
+  //Joining the room
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    console.log(clients);
+
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
+
+  socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
+
+  socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+    io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
+
+  socket.on(ACTIONS.CLEAR_TEXT, ({ roomId }) => {
+    socket.in(roomId).emit(ACTIONS.CLEAR_TEXT);
+  });
+
+  //Leaving the Room
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
+    });
+    delete userSocketMap[socket.id];
+    socket.leave();
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server Started Succesfully on ${PORT}...!`);
+});
